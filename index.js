6d70b1547e845bdfed68235a480a024e459f2dab
Daniela Romero <danyrp45@gmail.com>
2022-08-23 13:24:58 -0500

    almost done

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+const closeButton = document.querySelector(".close-button");
+const modal = document.querySelector(".modal");
 
+function openToggleModal(){    
+    modal.classList.add("show-modal");
+}
+
+function closeToggleModal(){
+    modal.classList.remove("show-modal")
+}
+
+closeButton.addEventListener("click", ()=>{
+    closeToggleModal();
+    iniciarJuego();    
+})
+
+///win modal
+const winButton = document.querySelector(".win-button");
+const win = document.querySelector(".win");
+
+function openToggleModalWin(){    
+    win.classList.add("show-modal");
+}
+
+function closeToggleModalWin(){
+    win.classList.remove("show-modal")
+}
+
+winButton.addEventListener("click", ()=>{
+    closeToggleModalWin();
+    iniciarJuego();
+})
+
+//////////////////////////CODIGO DE JUEGO////////////////////////////////////
 //width 950
 //height 713
 
@@ -11,11 +44,8 @@ let camarasInfo = document.getElementById("cams");
 let vidasInfo = document.getElementById("vidas");
 
 
-//boton inicio
-let botonIniciar = document.getElementsByTagName("button")[0];
-botonIniciar.addEventListener("click", () =>{
-   console.log("a jugar!")
-} ) 
+
+
 
  //imagenes
 const michiImg = new Image();
@@ -24,6 +54,10 @@ const competenciaImg = new Image();
 competenciaImg.src = "/images/competencia.png"
 const competencia2Img = new Image();
 competencia2Img.src = "/images/competencia2.png"
+const competencia3Img = new Image();
+competencia3Img.src = "/images/competencia3.png"
+const competencia4Img = new Image();
+competencia4Img.src = "/images/competencia4.png"
 const camaraImg = new Image();
 camaraImg.src = "/images/camara2.png"
 
@@ -35,8 +69,9 @@ class Cat{
         this.w = w;
         this.h = h;
         this.vida = vida;
-        this.color= color;
+        this.color= this.color
         this.imagen = imagen;
+        this.score = 0;
     }
 
     moverDerecha(){
@@ -62,9 +97,12 @@ class Cat{
     }
 
     dibujarse(){
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.2)';
         ctx.fillRect(this.x, this.y, this.w, this.h);
         ctx.drawImage(this.imagen,this.x, this.y, this.w, this.h);
+    }
+    recibirDaño(){
+
     }
   
 }
@@ -78,7 +116,7 @@ class Competencia{
         this.imagen = imagen;
     }
     dibujarse(){
-        ctx.fillStyle = "orange";
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.2)';
         ctx.fillRect(this.x,this.y,this.w,this.h);
         ctx.drawImage(this.imagen,this.x,this.y,this.w,this.h);
     }
@@ -93,7 +131,7 @@ class Camara{
         this.imagen = imagen;
     }
     dibujarse(){
-        ctx.fillStyle = "yellow";
+        ctx.fillStyle = 'rgba(255, 255, 255, 0)';
         ctx.fillRect(this.x,this.y,this.w,this.h);
         ctx.drawImage(this.imagen,this.x,this.y,this.w,this.h);
         
@@ -134,13 +172,13 @@ function escogeCam(camaras) {
 
 //INICIO
 function iniciarJuego(){
-    const michi = new Cat(30,590,150,120,7,"violet",michiImg);
-    const competencia = new Competencia(550,80,160,150,competencia2Img);
-    const competencia2 = new Competencia(700,170,150,90,competenciaImg);
-    const competencia3 = new Competencia(240,430,160,150,competencia2Img);
-    const competencia4 = new Competencia(680,550,150,90,competenciaImg);
+    const michi = new Cat(30,590,160,125,7,"violet",michiImg);
+    const competencia = new Competencia(530,100,180,120,competenciaImg);
+    const competencia2 = new Competencia(250,430,180,150,competencia2Img);
+    const competencia3 = new Competencia(60,160,130,120,competencia3Img);
+    const competencia4 = new Competencia(680,450,130,170,competencia4Img);
     const camara1= new Camara(430,250,150,120,camaraImg);
-    const camara2= new Camara(60,30,150,120,camaraImg);
+    const camara2= new Camara(50,30,150,120,camaraImg);
     const camara3= new Camara(450,500,150,120,camaraImg);
     const camara4= new Camara(780,100,150,120,camaraImg);
     const camaras = [camara1, camara2, camara3, camara4];
@@ -148,30 +186,55 @@ function iniciarJuego(){
     
     
     teclas(michi);
-    setInterval(() => escogeCam(camaras), 4000);
+    const intervalEscogeCam = setInterval(() => escogeCam(camaras), 4000);
 
 
-    setInterval(()=>{
+    const intervalJuego = setInterval(()=>{
         ctx.clearRect(0,0,950,713);
         vidasInfo.innerHTML = michi.vida;
+        camarasInfo.innerHTML = michi.score;
+
         michi.dibujarse();
         competenciasGatos.forEach((gato)=>{
             gato.dibujarse();
-            console.log(gato);
-
-            if(michi.x + michi.w >= gato.x && (michi.y >= gato.y + gato.h) ){
-                console.log("vida menos")
-                alert("");
+           if(
+                michi.x + michi.w >= gato.x &&
+                michi.y <= gato.y + gato.h &&
+                michi.y + michi.h >= gato.y &&
+                michi.x <= gato.x + gato.w
+            ){
+              michi.vida -= 1;
+              michi.x = 30;
+              michi.y = 590;
             }
 
+            if (michi.vida <= 0){
+                clearInterval(intervalJuego);
+                openToggleModal();
+            }
+
+            if(
+                michi.x + michi.w >= camaras[actualICam].x &&
+                michi.y <= camaras[actualICam].y + camaras[actualICam].h &&
+                michi.y + michi.h >= camaras[actualICam].y &&
+                michi.x <= camaras[actualICam].x + camaras[actualICam].w
+            ){
+                michi.score += 1;
+                escogeCam(camaras);
+            
+            }
+
+            if(michi.score >= 5){
+                clearInterval(intervalJuego);
+                console.log("Ganaste!")
+                openToggleModalWin();
+            }
         })
-        
         camaras[actualICam].dibujarse();
-
-      
-
-
     },1000/30)
+
+    
 }
 
 iniciarJuego();
+
